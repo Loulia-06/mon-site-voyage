b95f1e4d22549167aba9b4cd644609e02c534204
loulia <loulia.tsuiwaysagne@ynov.com>
2025-12-16 16:16:36 +0100

    version 2

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 export default function App() {
 
   const [selectedDestination, setSelectedDestination] = useState(null);
-
+  
   return (
     <div className="font-sans antialiased text-gray-900 bg-white min-h-screen flex flex-col">
      
